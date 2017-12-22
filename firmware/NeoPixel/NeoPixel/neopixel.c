0d3b0aaf423ec62686fe297f780ecdb11dbcafe0
Steve Mayze <smayze@yahoo.com>
2017-12-23 00:34:16 +0100

    Starting to tune the settings

--- firmware/NeoPixel/NeoPixel/neopixel.c
+++ firmware/NeoPixel/NeoPixel/neopixel.c
@@ -76,19 +76,19 @@ void neopixel_show()
 	    "rjmp .+0"                "\n\t" // 2    nop nop
 	    // Bit 5:
 	    "out  %[port] , %[hi]"    "\n\t" // 1    PORT = hi
-	      "mov  %[n2]   , %[lo]"    "\n\t" // 1    n2   = lo
-	      "out  %[port] , %[n1]"    "\n\t" // 1    PORT = n1
-	      "rjmp .+0"                "\n\t" // 2    nop nop
-	      "sbrc %[byte] , 4"        "\n\t" // 1-2  if(b & 0x10)
-	      "mov %[n2]   , %[hi]"    "\n\t" // 0-1   n2 = hi
-	      "out  %[port] , %[lo]"    "\n\t" // 1    PORT = lo
-	      "rjmp .+0"                "\n\t" // 2    nop nop
-	      // Bit 4:
-	      "out  %[port] , %[hi]"    "\n\t" // 1    PORT = hi
-	      "mov  %[n1]   , %[lo]"    "\n\t" // 1    n1   = lo
-	      "out  %[port] , %[n2]"    "\n\t" // 1    PORT = n2
-	      "rjmp .+0"                "\n\t" // 2    nop nop
-	      "sbrc %[byte] , 3"        "\n\t" // 1-2  if(b & 0x08)
+	    "mov  %[n2]   , %[lo]"    "\n\t" // 1    n2   = lo
+	    "out  %[port] , %[n1]"    "\n\t" // 1    PORT = n1
+	    "rjmp .+0"                "\n\t" // 2    nop nop
+	    "sbrc %[byte] , 4"        "\n\t" // 1-2  if(b & 0x10)
+	    "mov %[n2]   , %[hi]"    "\n\t" // 0-1   n2 = hi
+	    "out  %[port] , %[lo]"    "\n\t" // 1    PORT = lo
+	    "rjmp .+0"                "\n\t" // 2    nop nop
+	    // Bit 4:
+	    "out  %[port] , %[hi]"    "\n\t" // 1    PORT = hi
+	    "mov  %[n1]   , %[lo]"    "\n\t" // 1    n1   = lo
+	    "out  %[port] , %[n2]"    "\n\t" // 1    PORT = n2
+	    "rjmp .+0"                "\n\t" // 2    nop nop
+	    "sbrc %[byte] , 3"        "\n\t" // 1-2  if(b & 0x08)
 	      "mov %[n1]   , %[hi]"    "\n\t" // 0-1   n1 = hi
 	      "out  %[port] , %[lo]"    "\n\t" // 1    PORT = lo
 	      "rjmp .+0"                "\n\t" // 2    nop nop
--- firmware/NeoPixel/NeoPixel/neopixel.h
+++ firmware/NeoPixel/NeoPixel/neopixel.h
@@ -8,8 +8,8 @@
 
 #define neopixel_buffer_size neopixel_pixels * 3
 
-#define NEO_RED 0
-#define NEO_GREEN 1
+#define NEO_RED 1
+#define NEO_GREEN 0
 #define NEO_BLUE 2
 
 void neopixel_setPixel(uint8_t pixel, uint8_t red, uint8_t green, uint8_t blue);

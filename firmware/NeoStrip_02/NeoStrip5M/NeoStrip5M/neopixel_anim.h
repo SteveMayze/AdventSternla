c2f9089fac4b3223ab0efe5e3d4231c061636a8c
SteveMayze <smayze@yahoo.com>
2018-12-22 17:33:14 +0100

    Adds support for the 5m Neopixel Strip - 150

---
+++ firmware/NeoStrip_02/NeoStrip5M/NeoStrip5M/neopixel_anim.h
@@ -0,0 +1,17 @@
+/*
+ * neopixel_anim.h
+ *
+ * Created: 22/12/2018 15:46:02
+ *  Author: Steven
+ */ 
+
+
+#ifndef NEOPIXEL_ANIM_H_
+#define NEOPIXEL_ANIM_H_
+
+
+ void neo_anim_stars(uint8_t strip[]);
+
+
+
+#endif /* NEOPIXEL_ANIM_H_ */
